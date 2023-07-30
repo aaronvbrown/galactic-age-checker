3a80686954684d06e2fc3e4bca0aabe79190a92b
Aaron Brown <worldofaaron@hotmail.com>
2023-07-30 08:20:44 -0700

    pass first and second tests for convertedAge.  Green phase.

--- src/age-checker.js
+++ src/age-checker.js
@@ -1,5 +1,18 @@
 export default class GalacticAge {
-    constructor(age) {
-        this.earthAge = age;
+    constructor(earthAge, planet='earth') {
+        this.earthAge = earthAge;
+        this.planet = planet;
+        this.yearConversions = {
+            earth: 1,
+            mercury: .24,
+            venus: .62,
+            mars: 1.88,
+            jupiter: 11.86
+        }
+
+    }
+
+    convertedAge() {
+        return (this.yearConversions[this.planet] * this.earthAge);
     }
 }
