bc4d5ce87f80c9e09b2936f3315165e73f1c80c1
Aaron Brown <worldofaaron@hotmail.com>
2023-07-30 07:25:25 -0700

    red phase mercury age

--- __tests__/age-checker.test.js
+++ __tests__/age-checker.test.js
@@ -2,9 +2,14 @@ import GalacticAge from './../src/age-checker.js';
 
 describe(`GalacticAge`, () => {
 
-    test(`should return an object with earth age`, () => {
+    test(`should return an object with Earth age`, () => {
         let newGalacticAge = new GalacticAge(1);
         expect(newGalacticAge.earthAge).toEqual(1);
     });
-    
+
+    test(`should return an object with correct Mercury age`, () => {
+        let newGalacticAge = new GalacticAge(1);
+        expect(newGalacticAge.mercuryAge).toEqual(0.24);
+    });
+
 })
